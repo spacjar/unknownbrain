9d283af22e6c879ceca0e6ec6bc168dd04662e62
Jaroslav Špác <spacjar@gmail.com>
2022-05-08 13:31:30 +0200

    chore: bundle

--- public/app.js
+++ public/app.js
@@ -1,8 +1,10 @@
-// JS
-require('../src/scripts/main.js');
-//require('../src/scripts/threejs.js')
-
 // SCSS
 require('../src/scss/app.scss');
+
+// JS
+require('../src/scripts/main.js');
+//require('../src/scripts/scrollbar.js');
+require('../src/scripts/threejs.js');
+
 
 
