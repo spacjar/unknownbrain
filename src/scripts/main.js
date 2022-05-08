98642a136c6d1414d4c4da98bffff29404727d46
Jaroslav Špác <spacjar@gmail.com>
2022-05-08 12:35:46 +0200

    fix: main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,3 @@
-// const swup = new Swup();
-
-// Smooth Scroll JS
-// import Scrollbar from '../node_modules/smooth-scrollbar';
-// let options = {
-//     'damping': 0.2
-// };
-// Scrollbar.init(document.querySelector('#my-scrollbar'), options);
-
 // Preloader
 let preloader = document.getElementById("preloader");
 window.addEventListener("load", () => {
