18e2b2aa3a136a5b86217e966c14eae2842c2b48
Jaroslav Špác <spacjar@gmail.com>
2022-05-08 12:36:44 +0200

    feat: swup

---
+++ src/scripts/swup.js
@@ -0,0 +1,8 @@
+const swup = new Swup();
+
+// Smooth Scroll JS
+import Scrollbar from '../node_modules/smooth-scrollbar';
+let options = {
+    'damping': 0.2
+};
+Scrollbar.init(document.querySelector('#my-scrollbar'), options);
