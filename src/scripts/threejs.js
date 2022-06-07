bcc986ffad7995dc9ecc3829b06387cd4d6db9ca
Jaroslav Špác <spacjar@gmail.com>
2022-06-07 11:46:29 +0200

    fix: mask

--- src/scripts/threejs.js
+++ src/scripts/threejs.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
-const maskGltf = require('../../dist/ubmask-dist.gltf');
+const maskGltf = require('../../src/assets/models/ubmask.gltf');
 
 
 
